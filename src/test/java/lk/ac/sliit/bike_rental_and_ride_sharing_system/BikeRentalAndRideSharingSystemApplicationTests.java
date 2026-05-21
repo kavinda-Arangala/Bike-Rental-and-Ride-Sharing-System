035d3c697bb5d10b6c660511fd1988a41a15a703
Kavinda Arangala <IT25102099@my.sliit.lk>
2026-05-22 01:23:36 +0530

    Initial Commit

---
+++ src/test/java/lk/ac/sliit/bike_rental_and_ride_sharing_system/BikeRentalAndRideSharingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.bike_rental_and_ride_sharing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalAndRideSharingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
